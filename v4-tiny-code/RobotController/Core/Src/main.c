8ceca09af427ec0322990e9014ba350925847abc
路何求 <Deane_Chen@foxmail.com>
2023-06-30 15:50:04 +0800

    整合单色LED和多彩LED后调整了代码文件名

--- v4-tiny-code/RobotController/Core/Src/main.c
+++ v4-tiny-code/RobotController/Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "Led_ws2812.h"
+#include "led.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
