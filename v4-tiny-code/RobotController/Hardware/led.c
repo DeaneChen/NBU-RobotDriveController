ead02ce19da6d507f92918b3c9477c52e4de6fbe
darkarc <2350230863@qq.com>
2023-07-01 13:56:16 +0800

    修复led编译器版本导致的通信问题

--- v4-tiny-code/RobotController/Hardware/led.c
+++ v4-tiny-code/RobotController/Hardware/led.c
@@ -51,30 +51,62 @@ void FnLED_SetRGB(uint8_t id, uint8_t R, uint8_t G, uint8_t B, uint8_t is_show) 
  * @param  ns
  * @note   对于168MHz时钟，n为1时大概延时时间为370ns左右,3时为400ns
  */
-void delay_ns(__IO uint16_t ns) {
+
+
+void delay_ns(__IO uint16_t ns)
+{
     while (ns--)
         ;
 }
+
+/**
+ * ws2812 LED灯通信需要纳秒级延时通信
+ * 由于纳秒级别的延迟，受到编译器版本，优化等级的影响，请根据自己的实际情况，选择下面的设置。
+ * 编译器版本已自动判断，但优化等级需要自行选择。
+ * 目前已对led.c文件单独设置了o3优化等级，全局设置中，不会影响此文件优化等级。
+ */
+#if defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6000000)
+    // Arm Compiler 6
+		#if 1 //请根据自己设置的优化等级选择情况
+        // 优化级别为 O3
+			#define DATA0H_DELAY_NS 5
+			#define DATA0L_DELAY_NS 15
+			#define DATA1H_DELAY_NS 15
+			#define DATA1L_DELAY_NS 15
+		#else  //优化级别为 O0
+			#define DATA0H_DELAY_NS 1
+			#define DATA0L_DELAY_NS 7
+			#define DATA1H_DELAY_NS 6
+			#define DATA1L_DELAY_NS 7
+		#endif
+
+#else // Arm Compiler 5
+    #define DATA0H_DELAY_NS 1
+		#define DATA0L_DELAY_NS 12
+    #define DATA1H_DELAY_NS 9
+		#define DATA1L_DELAY_NS 9
+#endif
 
 /**
  * @brief  发送WS2812定义的“0”
  */
 void Data0() {
     LedIO_Set;
-    delay_ns(1);
+    delay_ns(DATA0H_DELAY_NS);
     LedIO_Reset;
-    delay_ns(12);
+    delay_ns(DATA0L_DELAY_NS);
 }
 
 /**
- * @brief  发送WS2812定义的“0”
+ * @brief  发送WS2812定义的“1”
  */
 void Data1() {
     LedIO_Set;
-    delay_ns(9);
+    delay_ns(DATA1H_DELAY_NS);
     LedIO_Reset;
-    delay_ns(9);
+    delay_ns(DATA1L_DELAY_NS);
 }
+
 
 /**
  * @brief  基于单极性归零码发送 WS2812 8位数据
@@ -101,7 +133,7 @@ void ws2812_show(void) {
     if (current_time - last_time < 0) {
         HAL_Delay(1); 
     }
-
+    last_time=current_time;
     for (uint8_t led = 0; led < LED_NUM; led++)
         for (uint8_t i = 0; i < 3; i++)
             send_data(buffer[led][i]);
